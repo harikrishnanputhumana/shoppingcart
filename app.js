713e9a54febf51ac5e7c582fc90fe02d1f8a0e2e
Harikrishnan <harikrishnanpnr001@gmail.com>
2020-10-10 13:25:48 +0530

    admin panel added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', userRouter);
-app.use('/users', adminRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
