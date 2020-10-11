d130926796f66326b4c89d57e717dbbe02c27e3f
Harikrishnan <harikrishnanpnr001@gmail.com>
2020-10-11 15:19:08 +0530

    successfully uploaded image via db and stored in public folder

--- routes/admin.js
+++ routes/admin.js
@@ -39,8 +39,17 @@ router.get('/add-product', function(req,res){
 router.post('/add-product',(req,res)=>{
   console.log(req.body);
   console.log(req.files.Image);
-  productHelpers.addProduct(req.body,(result)=>{
-    res.render("admin/add-product")
+  productHelpers.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    console.log(id);
+    image.mv('./public/product-images'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product")
+      }else{
+        console.log(err);
+      }
+    })
+    
   })
 })
 module.exports = router;
