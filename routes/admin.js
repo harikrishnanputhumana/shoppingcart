c46f55349bab8616e165ff89c48c36dfe3842cb4
Harikrishnan <harikrishnanpnr001@gmail.com>
2020-10-11 10:46:47 +0530

    view products file added

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index')
+  res.render('admin/view-products')
 });
 
 module.exports = router;
