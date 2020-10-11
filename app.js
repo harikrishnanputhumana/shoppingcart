1ea31372d3f405e9fc3675e272ec186f0727f4bd
Harikrishnan <harikrishnanpnr001@gmail.com>
2020-10-11 11:25:11 +0530

    fileupload just installed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 var hbs=require('express-handlebars')
+var fileUpload=require('express-fileupload')
 var app = express();
 
 // view engine setup
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload());
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
