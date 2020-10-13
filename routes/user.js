4d10f3c21ff737649823f8ad95d08c8b86af7812
Harikrishnan <harikrishnanpnr001@gmail.com>
2020-10-13 11:57:55 +0530

    login and signup form created

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,15 @@ router.get('/', function(req, res, next) {
     res.render('../user/view-products',{products})
 
   })
-  
-  
-});
 
+
+  
+})
+router.get('/login',(req,res)=>{
+  res.render('../user/login')
+})
+
+router.get('/signup',(req,res)=>{
+  res.render('../user/signup')
+})
 module.exports = router;
