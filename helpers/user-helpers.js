3c8678e8721a9849de7104a1d9e8f77e330ea280
Harikrishnan <harikrishnanpnr001@gmail.com>
2020-10-21 19:04:06 +0530

    cart adding and updating

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,6 +1,8 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
 const bcrypt=require('bcrypt')
+const { response } = require('express')
+var objectId=require('mongodb').ObjectID
 
 module.exports={
     doSignup:(userData)=>{
@@ -34,6 +36,30 @@ module.exports={
             }else{
                 console.log('user not exist')
                 resolve({status:false})
+            }
+        })
+    },
+    addToCart:(proId,userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+            if(userCart){
+                db.get().collection(collection.CART_COLLECTION)
+                .updateOne({user:objectId(userId)},
+                    {
+                        $push:{products:objectId(proId)}
+                    }
+                ).then((response)=>{
+                    resolve()
+                })
+
+            }else{
+                let cartObj={
+                    user:objectId(userId),
+                    products:[objectId(proId)]
+                }
+                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response)=>{
+                    resolve()
+                })
             }
         })
     }
